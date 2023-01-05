50924c249ee6dd5a377a2292800aee11bdfebd53
Hoon Kim <eloael@outlook.com>
2023-01-04 19:44:04 -0800

    added some syntax to tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,12 +5,50 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
   // be sure to include its associated Product data
+  include: [
+    {
+      model: Product,
+      through: ProductTag,
+      as: "products",
+    },
+  ],
+})
+.then((dbTagData) => res.json(dbTagData))
+.catch((err) => {
+  console.log(err);
+  res.status(500).json(err);
 });
+});
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
   // be sure to include its associated Product data
+  include: [
+    {
+      model: Product,
+      through: ProductTag,
+      as: "products",
+    },
+  ],
+})
+.then((dbTagData) => {
+  if (!dbTagData) {
+    res.status(404).json({ message: "No tag found with this id" });
+    return;
+  }
+  res.json(dbTagData);
+})
+.catch((err) => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.post('/', (req, res) => {
